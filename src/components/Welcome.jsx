2e8e214f381d34acce83ed6d414872477a5ddb1f
PrithviPPR8 <prithvirajpatil64@gmail.com>
2025-12-28 19:30:42 +0530

    Welcome 40:26

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,5 +1,17 @@
 import React, { useRef } from 'react'
 
+const renderText = (text, className, baseWeight = 400) => {
+    return [...text].map((char, i) => (   //this is the same as using text.split()
+        <span 
+            key={i} 
+            className={className} style={{
+            fontVariationSettings: `'whgt ${baseWeight}`}}
+        >
+            {char === " " ? '\u00A0' : char}   
+        </span>
+    ))
+}
+
 const Welcome = () => {
 
   const titleRef = useRef(null);
@@ -8,7 +20,11 @@ const Welcome = () => {
   return (
     <section id='welcome'>
         <p ref={subtitleRef}>Hey, I'm Prithvi! Welcome to my</p>
-        <h1>portfolio</h1>
+        <h1 ref={titleRef} className='mt-7'>portfolio</h1>
+
+        <div className='small-screen'>
+            <p>This portfolio is designed for desktop/tablet screens only.</p>
+        </div>
     </section>
   )
 }
