deb881c98ff308c3473c2de495f5a586286df43b
Prithviraj Patil <prithvirajpatil64@gmail.com>
2025-12-29 22:49:14 +0530

    Welcome 43:26

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,15 +1,28 @@
 import React, { useRef } from 'react'
 
+const FONT_WEIGHTS = {
+    subtitle: { min: 100, max: 400, default: 100 },
+    title: { min: 400, max: 900, default: 400 },
+}
+
 const renderText = (text, className, baseWeight = 400) => {
     return [...text].map((char, i) => (   //this is the same as using text.split()
         <span 
             key={i} 
-            className={className} style={{
-            fontVariationSettings: `'whgt ${baseWeight}`}}
+            className={className} 
+            style={{ fontVariationSettings: `'whgt ${baseWeight}` }}
         >
             {char === " " ? '\u00A0' : char}   
         </span>
     ))
+}
+
+const setupTextHover = (container, type) => {
+    if(!container) return;
+
+    const letters = container.querySelecterAll('span');
+
+    const { min, max, default: base } = FONT_WEIGHTS[type];
 }
 
 const Welcome = () => {
@@ -19,8 +32,20 @@ const Welcome = () => {
 
   return (
     <section id='welcome'>
-        <p ref={subtitleRef}>Hey, I'm Prithvi! Welcome to my</p>
-        <h1 ref={titleRef} className='mt-7'>portfolio</h1>
+        <p ref={subtitleRef}>
+            {renderText(
+                "Hey, I'm Prithvi! Welcome to my", 
+                'text-3xl font-georama', 
+                100
+            )}
+        </p>
+        <h1 ref={titleRef} className='mt-7'>
+            {renderText(
+                "portfolio", 
+                'text-9xl italic font-georama', 
+                400
+            )}
+        </h1>
 
         <div className='small-screen'>
             <p>This portfolio is designed for desktop/tablet screens only.</p>
