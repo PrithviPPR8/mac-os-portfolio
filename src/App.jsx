75cccefb539551ab39f17476c6e6054371b6f78a
Prithviraj Patil <prithvirajpatil64@gmail.com>
2026-01-07 16:44:47 +0530

    Implement Resume Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Navbar, Welcome, Dock } from '#components'
 import React from 'react'
 import gsap from 'gsap';
 import { Draggable } from 'gsap/Draggable'
-import { Safari, Terminal } from '#windows';
+import { Resume, Safari, Terminal } from '#windows';
 gsap.registerPlugin(Draggable);
 
 const App = () => {
@@ -14,6 +14,7 @@ const App = () => {
 
       <Terminal />
       <Safari />
+      <Resume />
     </main>
   )
 }
