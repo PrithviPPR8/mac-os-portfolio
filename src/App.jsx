3b20173b1d63600d61328924dc0e541cb77a23c5
Prithviraj Patil <prithvirajpatil64@gmail.com>
2026-01-07 12:08:25 +0530

    Implement Safari Browser Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Navbar, Welcome, Dock } from '#components'
 import React from 'react'
 import gsap from 'gsap';
 import { Draggable } from 'gsap/Draggable'
-import { Terminal } from '#windows';
+import { Safari, Terminal } from '#windows';
 gsap.registerPlugin(Draggable);
 
 const App = () => {
@@ -13,6 +13,7 @@ const App = () => {
       <Dock />
 
       <Terminal />
+      <Safari />
     </main>
   )
 }
