351ae7ebe2a321d93010639be311718fe66bc1da
Prithviraj Patil <prithvirajpatil64@gmail.com>
2026-01-09 13:19:27 +0530

    Implement Gallery section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Navbar, Welcome, Dock, Home } from '#components'
 import React from 'react'
 import gsap from 'gsap';
 import { Draggable } from 'gsap/Draggable'
-import { Finder, Resume, Safari, Terminal, Text, Image, Contact } from '#windows';
+import { Finder, Resume, Safari, Terminal, Text, Image, Contact, Photos } from '#windows';
 gsap.registerPlugin(Draggable);
 
 const App = () => {
@@ -19,6 +19,8 @@ const App = () => {
       <Text />
       <Image />
       <Contact />
+      <Photos />
+
       <Home />
     </main>
   )
